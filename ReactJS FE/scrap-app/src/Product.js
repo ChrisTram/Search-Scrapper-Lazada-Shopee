461a4d331246bab77c6480f99efdf0d596357dce
Chris TRAMIER <chris.tramier97@gmail.com>
2022-07-27 02:30:59 +0700

    Added and Setup CharJS

--- ReactJS FE/scrap-app/src/Product.js
+++ ReactJS FE/scrap-app/src/Product.js
@@ -27,40 +27,46 @@ export class Products extends Component {
     }
 
     createChar() {
-        const labels = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-          ];
-        
-          const data = {
-            labels: labels,
-            datasets: [{
-              label: 'My First dataset',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: [0, 10, 5, 2, 20, 30, 45],
-            }]
-          };
-        
-          const config = {
-            type: 'line',
-            data: data,
-            options: {}
-          };
-        
+
+
           const ctx = this.chartRef.current.getContext('2d');        
 
           if(this.myChart != null) this.myChart.destroy()
-
-        this.myChart = new Chart(
-            ctx,
-            config
-          );
-    }
+          this.myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Purple', 'Yellow', 'Green', 'Blue', 'Orange'],
+                datasets: [{
+                    label: '% Shopee',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                    ],
+                    borderWidth: 1
+                },
+                {
+                    label: '% Lazada',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(153, 102, 255, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(153, 102, 255, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        })}
 
     componentDidMount() {
         this.createChar()
