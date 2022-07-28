86bd0cd855799274d2b2ea408658241d81fcab3f
Chris TRAMIER <chris.tramier97@gmail.com>
2022-07-28 15:08:23 +0700

    Deployment setup

--- ShopApp/urls.py
+++ ShopApp/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from ShopApp import views
 
 urlpatterns=[
-    path('',views.productAPI),
     url(r'^products/$', views.productAPI),
     url(r'^product/([0-9]+)$',views.productAPI),
     path('searchProducts/<str:search>',views.searchProductsAPI)
