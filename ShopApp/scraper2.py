33f55a43c629b202f49cfab63f1f749892b978ff
Chris TRAMIER <chris.tramier97@gmail.com>
2022-07-28 13:07:27 +0700

    Deployment setup

--- ShopApp/scraper2.py
+++
@@ -1,9 +0,0 @@
-import requests
-from urllib.parse import urlencode
-
-list_of_urls = ['https://www.lazada.vn/catalog/?q=son+li']
-# for individual products
-for url in list_of_urls: 
-    params = {'api_key': 'a0bc68f74a763a1b622e6b3ba97567a0', 'url': url}
-    response = requests.get('http://api.scraperapi.com/', params=urlencode(params))
-    print(response.text)
