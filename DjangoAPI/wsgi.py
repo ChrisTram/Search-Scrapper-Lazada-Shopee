ad610723d3281fbd3e8ad9970f8c96c7dc219b20
Chris TRAMIER <chris.tramier97@gmail.com>
2022-07-28 15:58:24 +0700

    Deployment setup

--- DjangoAPI/wsgi.py
+++ DjangoAPI/wsgi.py
@@ -11,9 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoAPI.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'build/static'))
